0426c782a75a92b0eb3779e2ae577ae09564ea0a
William Pelletier <william_pelletier@hotmail.ca>
2018-03-19 15:10:14 -0400

    ajout du courriel et du téléphone

--- mes_modules/peupler/index.js
+++ mes_modules/peupler/index.js
@@ -14,9 +14,20 @@ const peupler_json = () => {
 		personneTab[k]['prenom'] = tableau.prenom[position];
     position = Math.floor(Math.random()*tableau.nom.length)
     personneTab[k]['nom'] = tableau.nom[position];
-    position = Math.floor(Math.random()*tableau.prenom.length)
-    personneTab[k]['prenom'] = tableau.prenom[position];
+    position = Math.floor(Math.random()*tableau.courriel.length)
+    personneTab[k]['courriel'] = personneTab[k]["prenom"] + "." + personneTab[k]["nom"] + "@" + tableau.courriel[position];
 	
+    let telephone = "";
+
+    for (var i = 0; i < 10; i++) {
+      
+      chiffre = Math.floor(Math.random()*10);
+      tel += chiffre;
+
+    }
+
+    personneTab[k]['telephone'] = telephone;
+
   }
 
   return(personneTab)

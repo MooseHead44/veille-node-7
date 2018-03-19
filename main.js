3dcdf689a5c3a82bb60bfc1e8a861b20ad6c948b
William Pelletier <william_pelletier@hotmail.ca>
2018-03-19 14:51:55 -0400

    ajout du git ignore et changements dans le code

--- main.js
+++ main.js
@@ -2,6 +2,22 @@
 
 const fs = require('fs');
 const peupler = require("./mes_modules/peupler");
+const express = require('express');
+const fs = require("fs");
+const app = express();
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient; // le pilote MongoDB
+const ObjectID = require('mongodb').ObjectID;
+
+const util = require("util");
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+/* on associe le moteur de vue au module «ejs» */
+app.set('view engine', 'ejs'); // générateur de template
+
+
 
 fs.readFile('toto.txt', (err,resultat) => {
 	if (err) console.log (err)
--- mes_modules/peupler/index.js
+++ mes_modules/peupler/index.js
@@ -1,20 +1,21 @@
 "use strict"
 const tableau  = require('./tableaux.js')
 
-const max = tableau.length
-
-console.log('max= ' + max)
 
 const peupler_json = () => {
 
-  let position 
-  let tabVille = []
-  for (let k=0 ; k<10; k++)
-	{
+  let position, chiffre 
+  let personneTab = []
+  for (let k=0 ; k<10; k++) {
+
+    personneTab[k] = {};
+
 		position = Math.floor(Math.random()*max)
-		tabVille.push(tableau[position])
-	}
-  return(tabVille)
+		personneTab[k]['prenom'] = tableaux.prenom[position];
+	
+  }
+
+  return(personneTab)
 }
 
 

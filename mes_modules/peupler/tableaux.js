1923c0bddbb6e46422386c212ae585646378d66c
William Pelletier <william_pelletier@hotmail.ca>
2018-03-19 15:09:51 -0400

    changement dans le tableau

--- mes_modules/peupler/tableaux.js
+++ mes_modules/peupler/tableaux.js
@@ -1,14 +1,41 @@
 "use strict";
-let tableau = [
-				"Montréal",
-				"Paris",
-				"Québec",
-				"Laval",
-				"Bruxelle",
-				"Ottawa",
-				"Calgary",
-				"Gaspé",
-				"Gatineau"
-				];
 
-module.exports = tableau;				
+let courriel = [
+	"gmail.com",
+	"hotmail.com",
+	"hotmail.ca",
+	"live.fr",
+	"yahoo.ca"
+];
+
+let prenom = [
+	"Bob",
+	"Roger",
+	"Bertrand",
+	"Simon",
+	"Rogeanne",
+	"Richard",
+	"Laurie",
+	"Anne",
+	"Rosalie",
+	"Marc",
+	"Maude",
+	"Félix",
+];
+
+let nom = [
+	"Pelletier",
+	"Tremblay",
+	"Gagnon",
+	"Marinier",
+	"Devault",
+	"Turcotte",
+	"Jolicoeur",
+	"Petit",
+	"Van Halen",
+	"Roy",
+];
+
+module.exports.courriel = courriel;
+module.exports.prenom = prenom;
+module.exports.nom = nom;
